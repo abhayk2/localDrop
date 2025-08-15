3ff6c2ed93aeb6add2da9b464823c045d5905a89
Abhay Kumar <abhayk176@gmail.com>
2025-08-15 19:41:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h1 class="text-5x

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        headline: ['Roboto', 'serif'],
         code: ['monospace'],
       },
       colors: {
